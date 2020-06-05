165decce21240136b3e1aef1fc4d50cda8517788
zch1999 <1901394767@qq.com>
2020-06-05 22:02:47 +0800

    服务端渲染同构流程

--- react-ssr/src/server/index.js
+++ react-ssr/src/server/index.js
@@ -3,22 +3,29 @@ import express from 'express';
 import React from 'react';
 // client dom 
 // 谁提供：虚拟 DOM
+import { StaticRouter } from 'react-router-dom';
+import { renderRoutes } from 'react-router-config';
 import { renderToString } from 'react-dom/server';
 import Header from '../components/Header.jsx';
+import Routes from '../Routes';
 
 
 const app = express();
 // static 目录做了静态资源的一个映射
 // koa-static
 app.use(express.static('static'))
-
 // ejs jsp jade vue-template:  if for 
 app.get('*', (req, res) => {
+  console.log(req.url);
   // 入口组件 jsx 
-  const App = (<Header />);
+  const App = (
+    <StaticRouter location={req.url}>
+      { renderRoutes(Routes) }
+    </StaticRouter>
+  );
   // jsx -> babel -> React.createElement()
   const htmlStr = renderToString(App);
-  console.log(htmlStr)
+  console.log(htmlStr);
   res.send(`<!DOCTYPE html>
   <html lang="en">
   <head>
@@ -30,9 +37,9 @@ app.get('*', (req, res) => {
     <div id="root">${htmlStr}</div>
     <script src="/index.js"></script>
   </body>
-  </html>`)
+  </html>`);
 })
 
 app.listen(3000, () => {
-  console.log('server is running 3000 port')
+  console.log('server is running 3000 port');
 })
